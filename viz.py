dc76d79f8927bc8b6929a59efa95d2e2d69ff809
Matthew Snell <83556530+MatthewLSnell@users.noreply.github.com>
2023-08-24 21:18:10 -0400

    updated pydeck_example w philly ex

--- viz.py
+++ viz.py
@@ -1,12 +1,8 @@
 # Imports
 from pathlib import Path
 import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-import matplotlib.pyplot as plt
-import seaborn as sns
 from typing import Union
+import pydeck as pdk
 
 def concat_csv_from_directory(directory: Union[str, Path]) -> pd.DataFrame:
     """Concatenate CSV files from the data directory that start with "CRASH"
@@ -38,3 +34,34 @@ def concat_csv_from_directory(directory: Union[str, Path]) -> pd.DataFrame:
 
 # Fetch crash data directly by passing the directory path to the function
 crash_data = concat_csv_from_directory(Path.cwd() / "data")
+
+# Columns to visualize
+columns_to_visualize = ["FATAL_COUNT", "INJURY_COUNT", "MCYCLE_DEATH_COUNT", "PED_DEATH_COUNT"]
+
+
+# Set a default metric for visualization, e.g., FATAL_COUNT
+metric = "FATAL_COUNT"
+
+layer = pdk.Layer(
+    'HexagonLayer',
+    crash_data,
+    get_position=['DEC_LONG', 'DEC_LAT'],
+    auto_highlight=True,
+    elevation_scale=50,
+    pickable=True,
+    extruded=True,
+    coverage=1,
+    elevation_range=[0, 5000],
+    get_elevation=metric,
+    get_fill_color='[255, (1-elevationValue/5000)*255, 0]'
+)
+
+view_state = pdk.ViewState(
+    longitude=-75.1652,
+    latitude=39.9526,
+    zoom=10,
+    pitch=40.5
+)
+
+r = pdk.Deck(layers=[layer], initial_view_state=view_state)
+r.to_html('demo.html')
